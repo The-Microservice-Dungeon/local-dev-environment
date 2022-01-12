acf1e85f085cbb5bb246f2568fd60f0839618abe
Thomas Raser <thomas.raser@th-koeln.de>
2022-01-12 17:17:15 +0100

    minor changes

--- classes/dev_env_factory.py
+++ classes/dev_env_factory.py
@@ -1,19 +1,76 @@
 import os, json
 from classes.dev_environment import Dev_Environment
 from classes.dungeon_services import Dungeon_Service
+import yaml
 
 
 class Dev_Env_Factory:
+    """ Factory class for development environments
+    """
 
-    def create_environment(self):
-        self.create_default_environment()
+    def create_environment(self, version):
+        """ Creates new Dev environment object
 
-    def create_default_environment(self):
-        if self.check_env_exists("default"):
-            return self.read_env_configs("default")
+        Args:
+            version (string): Version descriptor for dev environment
 
+        Returns:
+            [Dev_Environment]: [description]
+        """
+        with open(f"config/environments/env_setup_{version}.yml") as f:
+            config = yaml.load_all(f, Loader=yaml.FullLoader)
+            tmp_env = self.create_environment_object(config)
+            print(tmp_env.version)
+            tmp_env.save_config()
+            tmp_env.setup_environment()
+        return tmp_env
+
+    def create_from_available_version(self, environment_version):
+        """If a version is aleadry initialized, the object is red from a configuration file
+
+        Args:
+            environment_version ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        if self.check_env_exists(environment_version):
+            tmp_env = self.read_env_configs(environment_version)
+            tmp_env.setup_networks()
+            return tmp_env
+        else:
+            print(f"No JSON-file for environment:{environment_version}  found! Have you initialized it?")
+
+    def create_environment_object(self, config):
+        """Creates the env object
+
+        Args:
+            config ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        service_list = []
+        local_version = ""
+        for item in config:
+            for stuff, attribute in item.items():
+                if stuff == 'version':
+                    local_version = attribute
+                if stuff == 'Services':
+                    for key, value in attribute.items():
+                        service_list.append(Dungeon_Service(name=key, git_url=value['git_url'], branch=value['branch'],
+                                                            filepath=value['filepath']))
+            return Dev_Environment(local_version, service_list)
 
     def read_env_configs(self, environment):
+        """reads the env config
+
+        Args:
+            environment ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
         with open(f"config/environments/environment_{environment}.json") as jsonFile:
             json_object = json.load(jsonFile)
             jsonFile.close()
@@ -28,9 +85,17 @@ class Dev_Env_Factory:
         return tmp_env
 
     def check_env_exists(self, version):
+        """Checks if env version already exists
+
+        Args:
+            version ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
         if os.path.exists(f"config/environments/environment_{version}.json"):
-            print(("True"))
+
             return True
         else:
-            print(("False"))
+
             return False
--- classes/dev_environment.py
+++ classes/dev_environment.py
@@ -1,25 +1,5 @@
 import json, os
-import tools.helper_tools as tool
-
-from classes import dungeon_services
-
-
-def setup_default_environment():
-    """
-
-    Sets up the default environment with the latest versions of containers etc.
-
-    """
-    with open('config/networks.txt') as fp:
-        lines = fp.readlines()
-
-    lines = tool.strip_new_lines(lines)
-
-    print("Creating networks:")
-    for line in lines:
-        line = line.rstrip("\n")
-        print(f"Creating {line} network:")
-        os.system(f"docker network create {line} || true")
+import classes.tools.helper_tools as tool
 
 
 class Dev_Environment:
@@ -27,7 +7,7 @@ class Dev_Environment:
     class which contains the whole development environment
     """
 
-    def __init__(self, version="", services = []):
+    def __init__(self, version="", services=[]):
         """
         Creates new Dev env object
         Args:
@@ -36,43 +16,80 @@ class Dev_Environment:
         self.services = services
         self.version = version
 
+    def setup_networks(self):
+        """
 
-    def add_service(self, service):
-        self.services.append(service)
+        Sets up the default environment with the latest versions of containers etc.
+
+        """
+        with open('config/networks.txt') as fp:
+            lines = fp.readlines()
+
+        lines = tool.strip_new_lines(lines)
+
+        print("Creating networks:")
+        for line in lines:
+            line = line.rstrip("\n")
+            print(f"Creating {line} network:")
+            os.system(f"docker network create {line} || true")
 
     def save_config(self):
+        """Saves config to JSON file
+        """
         jsonStr = json.dumps(self, default=lambda o: o.__dict__, indent=4)
         print(jsonStr)
         f = open(f"config/environments/environment_{self.version}.json", "w+")
         f.write(jsonStr)
         f.close()
 
-    def add_default_services(self):
-        # Reads all service names
-        with open('config/services.txt') as fp:
-            lines = fp.readlines()
-
-        lines = tool.strip_new_lines(lines)
-        lines = tool.remove_service_from_list(lines)
-
-        # Adds the Default services
-        for line in lines:
-            self.services.append(dungeon_services.Dungeon_Service(line, filepath=f"deployment/{line}"))
-
     def run_environment(self):
-        if self.version == "default":
-            self.run_default_environment()
-
-    def run_default_environment(self):
+        """Runs the environment
+        """
         for service in self.services:
             os.system(f'cd {service.filepath} &&  docker-compose -f service-compose.yml up -d  --remove-orphans ')
 
     def setup_environment(self):
+        """ Environment setup
+        """
         if self.version == "default":
             print("Setting up default environment: ")
-            setup_default_environment()
+            self.setup_networks()
+        else:
+            self.setup_custom_environment()
+            self.setup_networks()
+
+    def update_all_containers(self):
+        """Updates all images
+        """
+        self.stop_environment()
+        for service in self.services:
+            os.system(f"cd {service.filepath} &&  docker-compose -f service-compose.yml pull")
+
+    def delete_containers_networks(self):
+        """deletes networks and containers
+        """
+        self.stop_environment()
+
+        with open('config/images.txt') as fp:
+            lines = fp.readlines()
+            lines = tool.strip_new_lines(lines)
+            for line in lines:
+                os.system(f"docker rmi {line}")
+
+    def setup_custom_environment(self):
+        """Sets up custom env
+        """
+        os.system("mkdir custom_environments")
+        os.system(f"mkdir custom_environments/version_{self.version}")
+        for service in self.services:
+            print(service.git_url)
+            os.system(f"cd custom_environments/version_{self.version} && git clone {service.git_url} ")
+            service.filepath = f"custom_environments/version_{self.version}/{service.name}"
+        self.save_config()
 
     def stop_environment(self):
+        """stops the environment
+        """
 
         for service in self.services:
             os.system(f"cd {service.filepath} &&  docker-compose -f service-compose.yml down")
